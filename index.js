b4dee052867092a2049cc5bc914bb567376f7295
Jamshid <itsjk0307>
2024-01-05 10:29:09 +0900

    .

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = 8080;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
