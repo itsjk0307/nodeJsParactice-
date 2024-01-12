7e9b389e0320e79058705b339fdee62c6be9149d
Jamshid Khaytbaev <itsjk0307@gmail.com>
2024-01-12 22:14:56 +0900

    .

--- index.js
+++ index.js
@@ -124,5 +124,5 @@ const server = http.createServer((req, res) => {
 const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  console.log("this is for testing2");
+  console.log("this is for testing3");
 });
